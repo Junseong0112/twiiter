ee9aac82bde105e1bed1cc0c57415b4e79ab4e7c
Jun Seong <drawbeat0112@gmail.com>
2024-02-22 19:47:00 +0900

    use firebase context

---
+++ src/Firebase/index.ts
@@ -0,0 +1,9 @@
+import FirebaseContext, {
+  WithFirebaseApi,
+  WithFirebaseApiProps,
+} from "./firebaseContext";
+import FirebaseApi from "./firebaseApi";
+
+export { FirebaseApi, FirebaseContext, WithFirebaseApi };
+
+export type { WithFirebaseApiProps };
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 import App from "./App";
+import { FirebaseApi, FirebaseContext } from "./Firebase";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <FirebaseContext.Provider value={new FirebaseApi()}>
+        <App />
+      </FirebaseContext.Provider>
+    </Provider>
   </React.StrictMode>
 );
