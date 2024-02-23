845ce481b5b428d1901a79e38fcce3147b6fd368
Jun Seong <drawbeat0112@gmail.com>
2024-02-23 18:20:04 +0900

    Loading State 추가하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { CircularProgress } from "@mui/material";
 import { useEffect } from "react";
 import { WithFirebaseApiProps, WithFirebaseApi } from "./Firebase";
 import Header from "./components/Header";
-import { useAppDispatch } from "./redux/hooks";
+import { useAppDispatch, useAppSelector } from "./redux/hooks";
+import { RootState } from "./redux/store";
 import { setUserId } from "./redux/useSlice";
 
+const isLoadingState = (state: RootState): boolean => {
+  return state.user.userId === undefined;
+};
+
 function App(props: WithFirebaseApiProps) {
+  const isLoading = useAppSelector(isLoadingState);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
@@ -16,6 +23,10 @@ function App(props: WithFirebaseApiProps) {
       }
     });
   }, []);
+
+  if (isLoading) {
+    return <CircularProgress sx={{ margin: "auto" }} />;
+  }
 
   return (
     <>
