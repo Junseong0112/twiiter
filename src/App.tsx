cd179796e338d1978fec0335a17683d21c3b9437
Jun Seong <drawbeat0112@gmail.com>
2024-02-23 18:16:41 +0900

    Header Componet 배치

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
+import { useEffect } from "react";
+import { WithFirebaseApiProps, WithFirebaseApi } from "./Firebase";
 import Header from "./components/Header";
+import { useAppDispatch } from "./redux/hooks";
+import { setUserId } from "./redux/useSlice";
 
-function App() {
+function App(props: WithFirebaseApiProps) {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    return props.firebaseApi.onAuthStateChanged((user) => {
+      if (user) {
+        dispatch(setUserId(user.uid));
+      } else {
+        dispatch(setUserId(null));
+      }
+    });
+  }, []);
+
   return (
     <>
       <Header />
@@ -8,4 +24,4 @@ function App() {
   );
 }
 
-export default App;
+export default WithFirebaseApi(App);
